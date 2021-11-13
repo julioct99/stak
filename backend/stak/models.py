5a2977f83d89cca4823962b1a8e2e7522c61b370
julioct99 <jcasallot01@gmail.com>
2021-11-13 13:23:27 +0100

    Backend: Initial basic models

--- backend/stak/models.py
+++ backend/stak/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TransactionCategory(models.Model):
+    name = models.CharField(max_length=75)
+
+
+class TransactionSubcategory(models.Model):
+    category = models.ForeignKey(TransactionCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=75)
+
+
+class Wallet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75)
+    balance = models.DecimalField(decimal_places=2, max_digits=16)
+
+
+class Transaction(models.Model):
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(
+        TransactionSubcategory, on_delete=models.SET_NULL, null=True
+    )
+    amount = models.DecimalField(decimal_places=2, max_digits=16)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
