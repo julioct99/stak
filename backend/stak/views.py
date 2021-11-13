c19b2c946fa75ec36ba433b6861c9757acd10770
julioct99 <jcasallot01@gmail.com>
2021-11-13 13:34:15 +0100

    Backend: Initial basic viewsets

--- backend/stak/views.py
+++ backend/stak/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+    Transaction,
+    TransactionCategory,
+    TransactionSubcategory,
+    User,
+    Wallet,
+)
+from .serializers import (
+    TransactionCategorySerializer,
+    TransactionSerializer,
+    TransactionSubcategorySerializer,
+    UserSerializer,
+    WalletSerializer,
+)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
+class TransactionCategoryViewSet(viewsets.ModelViewSet):
+    queryset = TransactionCategory.objects.all()
+    serializer_class = TransactionCategorySerializer
+
+
+class TransactionSubcategorySerializer(viewsets.ModelViewSet):
+    queryset = TransactionSubcategory.objects.all()
+    serializer_class = TransactionSubcategorySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
