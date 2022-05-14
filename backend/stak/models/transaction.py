49d67cc6286f75c1a0bf44c9e50e0aad56c09f9f
julioct99 <jcasallot01@gmail.com>
2022-05-14 19:03:22 +0200

    Fix volumes, recover lost code and create postgres container

--- backend/stak/models/transaction.py
+++ backend/stak/models/transaction.py
@@ -4,12 +4,14 @@ from django.dispatch import receiver
 from stak.models.transaction_subcategory import TransactionSubcategory
 from stak.models.wallet import Wallet
 
+
 class Transaction(models.Model):
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     subcategory = models.ForeignKey(
         TransactionSubcategory, on_delete=models.SET_NULL, null=True
     )
     amount = models.DecimalField(decimal_places=2, max_digits=16)
+    description = models.CharField(max_length=100, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
 
